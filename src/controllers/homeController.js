d08f92db8570ac556a27e7993d9c8e58cfba2129
Nguyễn Minh Quân <142667586+nguyenminhquan2204@users.noreply.github.com>
2025-06-30 00:51:55 +0700

    Edit-user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,9 @@
 import db from '../models/index';
+import CRUDServices from '../services/CRUDServices';
 
 let getHomePage = async (req, res) => {
    try {
-      let data = await db.User.findAll(); 
+      let data = await db.User.findAll();
 
       return res.render('homepage.ejs', {
          data: JSON.stringify(data),
@@ -13,7 +14,53 @@ let getHomePage = async (req, res) => {
    }
 }
 
+let getCRUD = async (req, res) => {
+   return res.render('crud.ejs')
+}
+
+let postCRUD = async (req, res) => {
+   // console.log(req.body);
+   let message = await CRUDServices.createNewUser(req.body);
+   return res.send(message);
+}
+
+let displayGetCRUD = async (req, res) => {
+   let data = await CRUDServices.getAllUser();
+
+   return res.render('displayCRUD.ejs', {
+      dataTable: data
+   })
+}
+
+let getEditCRUD = async (req, res) => {
+   let userId = req.query.id;
+
+   if (userId) {
+      let userData = await CRUDServices.getUserInfoById(userId);
+
+      return res.render('editCRUD.ejs', {
+         user: userData
+      });
+   } else {
+      return res.send("User not Found");
+   }
+}
+
+let putCRUD = async (req, res) => {
+   let data = req.body;
+   let allUsers = await CRUDServices.updateUserData(data);
+
+   return res.render('displayCRUD.ejs', {
+      dataTable: allUsers
+   });
+}
+
 module.exports = {
    getHomePage: getHomePage,
+   getCRUD: getCRUD,
+   postCRUD: postCRUD,
+   displayGetCRUD: displayGetCRUD,
+   getEditCRUD: getEditCRUD,
+   putCRUD: putCRUD,
 
 }
--- src/route/web.js
+++ src/route/web.js
@@ -10,6 +10,16 @@ let initWebRoutes = (app) => {
 
    router.get("/home", homeController.getHomePage);
 
+   router.get("/crud", homeController.getCRUD);
+
+   router.post("/post-crud", homeController.postCRUD);
+
+   router.get('/get-crud', homeController.displayGetCRUD);
+
+   router.get('/edit-crud', homeController.getEditCRUD);
+
+   router.post('/put-crud', homeController.putCRUD);
+
    return app.use("/", router);
 }
 
