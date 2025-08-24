a83c349ae1ad0b6ec38f2647f530d37f3bf5b685
Nguyễn Minh Quân <142667586+nguyenminhquan2204@users.noreply.github.com>
2025-08-24 13:55:50 +0700

    API booking appointment

---
+++ src/controllers/patientController.js
@@ -0,0 +1,20 @@
+import patientService from '../services/patientService';
+
+let postBookAppointment = async (req, res) => {
+   try {
+      let infor = await patientService.postBookAppointment(req.body);
+      
+      return res.status(200).json(infor);
+   } catch (error) {
+      console.log(error);
+      return res.status(200).json({
+         errorCode: -1,
+         errorMessage: 'Error from server ...'
+      })
+   }
+}
+
+module.exports = {
+   postBookAppointment: postBookAppointment
+
+}
---
+++ src/services/patientService.js
@@ -0,0 +1,57 @@
+import db from "../models/index";
+require('dotenv').config();
+
+let postBookAppointment = (data) => {
+   return new Promise(async (resolve, reject) => {
+      try {
+         if (!data.email || !data.doctorId || !data.timeType || !data.date) {
+            resolve({
+               errorCode: 1,
+               errorMessage: 'Missing required parameters !'
+            })
+         } else {
+            // upsert patient
+            let user = await db.User.findOrCreate({
+               where: {
+                  email: data.email
+               },
+               defaults: {
+                  email: data.email,
+                  roleId: 'R3'
+               },
+               raw: true
+            })
+
+            // Create a booking record
+            // console.log('data user', user[0]);
+            if (user && user[0]) {
+               await db.Booking.findOrCreate({
+                  where: {
+                     patientId: user[0].id
+                  },
+                  defaults: {
+                     statusId: 'S1',
+                     doctorId: data.doctorId,
+                     patientId: user[0].id,
+                     date: data.date,
+                     timeType: data.timeType,
+                  },
+               })
+
+            }
+
+            resolve({
+               errorCode: 0,
+               errorMessage: 'Save infor patient succeed!',
+            })
+         }
+      } catch (error) {
+         reject(error);
+      }
+   })
+}
+
+module.exports = {
+   postBookAppointment: postBookAppointment
+
+}
